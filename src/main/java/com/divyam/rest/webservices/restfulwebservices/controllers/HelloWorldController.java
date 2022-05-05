26c05eed3066cf4bc83d674bb5bb680704c40f77
Divyam14 <divyam.dembla@gmail.com>
2022-05-05 14:14:27 +0530

    Advanced Restful Services Features Hal Actuator Swagger(OpenApi) Content negotiation Internationalization

--- src/main/java/com/divyam/rest/webservices/restfulwebservices/controllers/HelloWorldController.java
+++ src/main/java/com/divyam/rest/webservices/restfulwebservices/controllers/HelloWorldController.java
@@ -1,11 +1,19 @@
 package com.divyam.rest.webservices.restfulwebservices.controllers;
 
 import com.divyam.rest.webservices.restfulwebservices.models.HelloWorldBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 //Controller
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    MessageSource messageSource;
 
 
     //@RequestMapping(method = RequestMethod.GET , path = "/hello-world")
@@ -22,6 +30,19 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
         return new HelloWorldBean("Hello World, Your name is "+name);
+    }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized(
+            @RequestHeader(name = "Accept-Language",required = false)Locale locale
+            ){
+        return messageSource.getMessage("good.morning.message",null,"Default Message",locale);
+    }
+
+
+    @GetMapping(path = "/hello-world-internationalized2")
+    public String helloWorldInternationalized2(){
+        return messageSource.getMessage("good.morning.message",null,"Default Message", LocaleContextHolder.getLocale());
     }
 
 }
